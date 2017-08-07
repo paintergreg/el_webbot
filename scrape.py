eca2c0ba4be40c84a60a2237348e58141cb3bbb2
Greg Painter <greg.painter@gmail.com>
2017-08-07 12:02:40 -0600

    Application is functional.  Documenting code and adding user interface.

--- scrape.py
+++ scrape.py
@@ -4,6 +4,9 @@
 from bs4 import BeautifulSoup
 import requests
 import os
+from shutil import rmtree
+
+BASEDIR = './AAATemp/'
 
 #
 # Start at the http://www.emblibrary.com/EL/New.aspx
@@ -14,27 +17,26 @@ import os
 def findLinks(url):
     r = requests.get(url)
     soup = BeautifulSoup(r.content, 'html.parser')
+    #
+    # Find all the anchor tags that reference design details.
+    # There may be different sizes for each design.  Follow each
+    # size reference.
+    #
     for link in soup.select('span.sizeSpan a'):
         href = link.get('href')
         params = href.split('=')  # params[2] is the productID
-        # print(params[2])
-        # print(href)
-        designDetail(params[2], href)
-        # print(type(href))
-        # if(type(href) is str):
-        #     if href.find('ProductID') != -1:
-        #         print(link.get('href'))
+        designDetail(params[2], href)  # This function follows design reference
 
 def designDetail(productID, url):
-    print(productID, url)
+    # print(productID, url)
     r = requests.get(url)
     soup = BeautifulSoup(r.content, 'html.parser')
     anchor = soup.select('a#ColorChangeLink')
     href = anchor[0].get('href')
-    findParams(productID, href)
+    downloadPDF(productID, href)
 
-def findParams(productID, href):
-    print(productID, href)
+def downloadPDF(productID, href):
+    # print(productID, href)
     chunk_size = 2048
     payload = {}
     payload['productID'] = productID
@@ -43,20 +45,29 @@ def findParams(productID, href):
     url = 'http://www.emblibrary.com/EL/' + href
     r = requests.get(url)
     soup = BeautifulSoup(r.content, 'html.parser')
+    productName = soup.select('table.content-item.info-table.padded.no-border > tr td:nth-of-type(2)')[0].text.strip()
+    print(productName, productID)
     # print(soup.prettify())
     hidden_tags = soup.findAll(attrs={"type" : "hidden"})
-    print(hidden_tags[0]['value'])
+    # print(hidden_tags[0]['value'])
     for tag in hidden_tags:
-        print(tag['value'], tag['type'], tag['name'])
+        # print(tag['value'], tag['type'], tag['name'])
         postData[tag['name']] = tag['value']
     r = requests.post(url, params=payload, data=postData)
-    print(r.url)
-    filePath = './AAATemp/' + productID + '.pdf'
-    with open(filePath, 'wb') as fd:
+    dirPath = './AAATemp/' + productID + ' ' + productName + '/'
+    os.makedirs(dirPath, exist_ok=True)
+    filePath =  productID + ' ' + productName + '.pdf'
+    with open(dirPath + filePath, 'wb') as fd:
         for chunk in r.iter_content(chunk_size):
             fd.write(chunk)
 
+#
+# Make sure the folder to store the PDF files is gone.
+# The folder must be empty to begin or the PDF file will build.
+#
+def folderInitialize():
+    rmtree(BASEDIR)
 
 if __name__ == "__main__":
-    findLinks('http://www.emblibrary.com/EL/New.aspx')
-    # findParams()
+    folderInitialize()
+    # findLinks('http://www.emblibrary.com/EL/New.aspx')
