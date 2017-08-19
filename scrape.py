63517152037f330579b395b7eb4acfbb72894be2
Greg Painter <greg.painter@gmail.com>
2017-08-19 14:22:01 -0600

    Added validation for date entered by users. Formats acceptable DD-MM-YY.  WIth one or two digit day and month or two or four digit year.  Either - or / delimiter.

--- scrape.py
+++ scrape.py
@@ -3,10 +3,12 @@
 
 from bs4 import BeautifulSoup
 import requests
+import re
 import os
 import sys
 from shutil import rmtree
 import signal
+from datetime import datetime
 
 BASEDIR = './AAATemp/'
 defaultURL = "http://www.emblibrary.com/EL/New.aspx"
@@ -83,7 +85,7 @@ def downloadPDF(productID, href):
 #####################################################################
 #
 # Make sure the folder to store the PDF files is empty.
-# The folder must be empty to begin or the PDF file will build.
+# The folder must be empty to begin or the PDF files will build.
 #
 def folderInitialize():
     if os.path.exists(BASEDIR):
@@ -100,16 +102,40 @@ def signal_handler(signal, frame):
 
 #####################################################################
 #
-# User input to select the week to download.  User enters MMDDYY or
-# and empty string.
+# User input to select the week to download.  User enters DD-MM-YY or
+# and empty string.  Embrodiery Library accepts the date at MMDDYY.
 #
 def keyBoardInput():
     url = defaultURL
-    i = input("Enter MMDDYY or Return: ")
+    i = input("Enter Date as: DD-MM-YY or DD/MM/YY or Return for today: ")
     if i != "":
-        url += "?date=" + i
+        date = validDate(i)
+        if date is not None:
+            url += "?date=" + date
+        else:
+            return None
     return(url)
 
+#####################################################################
+# Validate the user entered a string that can be converted to a
+# valid date.
+#
+def validDate(datestring):
+    regex = re.compile(r"""
+                (\d{1,2})[/-]
+                (\d{1,2})[/-]
+                (\d{2,4})
+                """, re.VERBOSE)
+    mat = regex.search(datestring)
+    # print(mat.groups()[-1::-1])
+    try:
+        if mat is not None:
+            now = datetime(*(map(int, mat.groups()[-1::-1])))
+            print(now)
+            return now.strftime("%m%d%y")
+    except ValueError:
+        print("DateError")
+    return None
 
 #####################################################################
 # Control the main loop.
@@ -118,4 +144,8 @@ if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal_handler)
     folderInitialize()
     url = keyBoardInput()
-    findLinks(url)
+    if url is None:
+        print("Problem entering date.")
+    else:
+        print(url)
+        findLinks(url)
