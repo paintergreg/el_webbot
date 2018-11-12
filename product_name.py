b64cd84b59dc44ebc98d85f13a4b9f4aecea410f
Greg Painter <greg.painter@gmail.com>
2018-11-12 15:28:31 -0700

    Implemented more file name modifications

--- product_name.py
+++ product_name.py
@@ -32,47 +32,52 @@ def modify_product_name(i):
     if n.find(' with ') >= 0:
         n = n.replace(' with ', ' w ')
 
-    if n.find('(Applique)') >= 0:
-        n = n.replace('(Applique)', '(App)')
+    # Keep this in order
+    if n.find('Crafty Cut Applique') >= 0:
+        n = n.replace('Crafty Cut Applique', 'CCA')
 
-    if n.find('(Vinyl Applique)') >= 0:
-        n = n.replace('(Vinyl Applique)', '(VA)')
+    if n.find('Crafty Cut') >= 0:
+        n = n.replace('Crafty Cut', 'CC')
+
+    if n.find('Vinyl Applique') >= 0:
+        n = n.replace('Vinyl Applique', 'VA')
+
+    if n.find('Applique') >= 0:
+        n = n.replace('Applique', 'App')
+    # End of Keep these in order
 
     if n.find('Christmas') >= 0:
         n = n.replace('Christmas', 'Xmas')
 
-    if n.find('(Whitework)') >= 0:
-        n = n.replace('(Whitework)', '(WW)')
+    if n.find('Whitework') >= 0:
+        n = n.replace('Whitework', 'WW')
 
-    if n.find('(Vintage)') >= 0:
-        n = n.replace('(Vintage)', '(V)')
+    if n.find('Vintage') >= 0:
+        n = n.replace('Vintage', 'V')
 
     if n.find(' and ') >= 0:
         n = n.replace(' and ', ' & ')
 
-    if n.find('(Crafty Cut)') >= 0:
-        n = n.replace('(Crafty Cut)', '(CC)')
+    if n.find('Organza') >= 0:
+        n = n.replace('Organza', 'O')
 
-    if n.find('(Organza)') >= 0:
-        n = n.replace('(Organza)', '(O)')
+    if n.find('Puff Foam') >= 0:
+        n = n.replace('Puff Foam', 'PF')
 
-    if n.find('(Puff Foam)') >= 0:
-        n = n.replace('(Puff Foam)', '(PF)')
+    if n.find('Thick Thread') >= 0:
+        n = n.replace('Thick Thread', 'TT')
 
-    if n.find('(Thick Thread)') >= 0:
-        n = n.replace('(Thick Thread)', '(TT)')
+    if n.find('Cardstock') >= 0:
+        n = n.replace('Cardstock', 'CS')
 
-    if n.find('(Cardstock)') >= 0:
-        n = n.replace('(Cardstock)', '(CS)')
+    if n.find('Redwork') >= 0:
+        n = n.replace('Redwork', 'RW')
 
-    if n.find('(Redwork)') >= 0:
-        n = n.replace('(Redwork)', '(RW)')
+    if n.find('Embossed') >= 0:
+        n = n.replace('Embossed', 'EM')
 
-    if n.find('(Embossed)') >= 0:
-        n = n.replace('(Embossed)', '(EM)')
-
-    if n.find('(Double Run)') >= 0:
-        n = n.replace('(Double Run)', '(DR)')
+    if n.find('Double Run') >= 0:
+        n = n.replace('Double Run', 'DR')
 
     if n.find('Ornament') >= 0:
         n = n.replace('Ornament', 'Ornie')
@@ -94,6 +99,210 @@ def modify_product_name(i):
 
     if n.find('Accent') >= 0:
         n = n.replace('Accent', 'Acnt')
+
+    if n.find('Etching') >= 0:
+        n = n.replace('Etching', 'Etch')
+
+    if n.find('Goldwork') >= 0:
+        n = n.replace('Goldwork', 'GW')
+
+    if n.find('Halloween') >= 0:
+        n = n.replace('Halloween', 'Halwen')
+
+    if n.find('Not-So-Spooky') >= 0:
+        n = n.replace('Not-So-Spooky', 'Not Spooky')
+
+    if n.find('Bountiful') >= 0:
+        n = n.replace('Bountiful', 'Bntfl')
+
+    if n.find('Pumpkin') >= 0:
+        n = n.replace('Pumpkin', 'Pmpkn')
+
+    if n.find('Harvest') >= 0:
+        n = n.replace('Harvest', 'Hvst')
+
+    if n.find('Autumn') >= 0:
+        n = n.replace('Autumn', 'Atmn')
+
+    if n.find('Topiary') >= 0:
+        n = n.replace('Topiary', 'Tpry')
+
+    if n.find('Jacobean') >= 0:
+        n = n.replace('Jacobean', 'Jbean')
+
+    if n.find('Enchanted') >= 0:
+        n = n.replace('Enchanted', 'Echnt')
+
+    if n.find('Blossoming') >= 0:
+        n = n.replace('Blossoming', 'Blosm')
+
+    if n.find('Medallion') >= 0:
+        n = n.replace('Medallion', 'Mdln')
+
+    if n.find('Woodland') >= 0:
+        n = n.replace('Woodland', 'Wlnd')
+
+    if n.find('Fantastic') >= 0:
+        n = n.replace('Fantastic', 'Fntstc')
+
+    if n.find('Marvelous') >= 0:
+        n = n.replace('Marvelous', 'Mrvls')
+
+    if n.find('Top of the Line -') >= 0:
+        n = n.replace('Top of the Line -', 'TOL')
+
+    if n.find('Blooming') >= 0:
+        n = n.replace('Blooming', 'Blmg')
+
+    if n.find('Dinosaurs') >= 0:
+        n = n.replace('Dinosaurs', 'Dnsrs')
+
+    if n.find('Lightening') >= 0:
+        n = n.replace('Lightening', 'Lghtng')
+
+    if n.find('Trapunto') >= 0:
+        n = n.replace('Trapunto', 'Tpnto')
+
+    if n.find('Southwestern') >= 0:
+        n = n.replace('Southwestern', 'Swtrn')
+
+    if n.find('Delightful') >= 0:
+        n = n.replace('Delightful', 'Dltfl')
+
+    if n.find('Ballerina') >= 0:
+        n = n.replace('Ballerina', 'Blnra')
+
+    if n.find('Bouquet') >= 0:
+        n = n.replace('Bouquet', 'Bqt')
+
+    if n.find('Sunflower') >= 0:
+        n = n.replace('Sunflower', 'Snflwr')
+
+    if n.find('Schematic') >= 0:
+        n = n.replace('Schematic', 'Scmtc')
+
+    if n.find('Balloon') >= 0:
+        n = n.replace('Balloon', 'Baln')
+
+    if n.find('Impossible') >= 0:
+        n = n.replace('Impossible', 'Impsbl')
+
+    if n.find('Hummingbird') >= 0:
+        n = n.replace('Hummingbird', 'Hmgbrd')
+
+    if n.find('Snowflake') >= 0:
+        n = n.replace('Snowflake', 'Snflk')
+
+    if n.find('Wonderland') >= 0:
+        n = n.replace('Wonderland', 'Wdrlnd')
+
+    if n.find('Monarch') >= 0:
+        n = n.replace('Monarch', 'Mnrh')
+
+    if n.find('Dazzling') >= 0:
+        n = n.replace('Dazzling', 'Dzlng')
+
+    if n.find('Delicate') >= 0:
+        n = n.replace('Delicate', 'Dlcte')
+
+    if n.find('Monday') >= 0:
+        n = n.replace('Monday', 'Mon')
+
+    if n.find('Tuesday') >= 0:
+        n = n.replace('Tuesday', 'Tue')
+
+    if n.find('Wednesday') >= 0:
+        n = n.replace('Wednesday', 'Wed')
+
+    if n.find('Thursday') >= 0:
+        n = n.replace('Thursday', 'Thu')
+
+    if n.find('Friday') >= 0:
+        n = n.replace('Friday', 'Fri')
+
+    if n.find('Saturday') >= 0:
+        n = n.replace('Saturday', 'Sat')
+
+    if n.find('Sunday') >= 0:
+        n = n.replace('Sunday', 'Sun')
+
+    if n.find('Baroque') >= 0:
+        n = n.replace('Baroque', 'Brqe')
+
+    if n.find('Goldfinch') >= 0:
+        n = n.replace('Goldfinch', 'Gldfnh')
+
+    if n.find('Botanicals') >= 0:
+        n = n.replace('Botanicals', 'Btncls')
+
+    if n.find('Goldfish') >= 0:
+        n = n.replace('Goldfish', 'Gldfsh')
+
+    if n.find('Kaleidoscope') >= 0:
+        n = n.replace('Kaleidoscope', 'Kldscpe')
+
+    if n.find('Ironwork') >= 0:
+        n = n.replace('Ironwork', 'Irnwrk')
+
+    if n.find('Moroccan') >= 0:
+        n = n.replace('Moroccan', 'Mrcn')
+
+    if n.find('Quilting') >= 0:
+        n = n.replace('Quilting', 'Qltng')
+
+    if n.find('Nouveau') >= 0:
+        n = n.replace('Nouveau', 'Nvu')
+
+    if n.find('Japanese') >= 0:
+        n = n.replace('Japanese', 'Jpnse')
+
+    if n.find('Dragonfly') >= 0:
+        n = n.replace('Dragonfly', 'Drgfl')
+
+    if n.find('Birdhouse') >= 0:
+        n = n.replace('Birdhouse', 'Brdhs')
+
+    if n.find('Majestic') >= 0:
+        n = n.replace('Majestic', 'Mjstc')
+
+    if n.find('American') >= 0:
+        n = n.replace('American', 'Amrcn')
+
+    if n.find('Exquisite') >= 0:
+        n = n.replace('Exquisite', 'Exqst')
+
+    if n.find('Lighthouse') >= 0:
+        n = n.replace('Lighthouse', 'Lthse')
+
+    if n.find('Starfish') >= 0:
+        n = n.replace('Starfish', 'Strfs')
+
+    if n.find('Seafood') >= 0:
+        n = n.replace('Seafood', 'Sefd')
+
+    if n.find('Adventure') >= 0:
+        n = n.replace('Adventure', 'Advntr')
+
+    if n.find('Patchwork') >= 0:
+        n = n.replace('Patchwork', 'Ptwrk')
+
+    if n.find('Free-Flying') >= 0:
+        n = n.replace('Free-Flying', 'FF')
+
+    if n.find('Rainforest') >= 0:
+        n = n.replace('Rainforest', 'Rnfrst')
+
+    if n.find('Madagascan') >= 0:
+        n = n.replace('Madagascan', 'Mdgscn')
+
+    if n.find('Forget-Me-Not') >= 0:
+        n = n.replace('Forget-Me-Not', 'FMN')
+
+    if n.find('Dragonflies') >= 0:
+        n = n.replace('Dragonflies', 'Drgnfls')
+
+    if n.find(' - ') >= 0:
+        n = n.replace(' - ', ' ')
 
     n = n.strip()
     return n
@@ -127,6 +336,20 @@ if __name__ == "__main__":
             'Poinsettia is a flower',
             'Paint a Poinsettia with Watercolor',
             'Accent the Border with Squares',
+            'Not-So-Spooky Bountiful Harvest Pumpkin in Topiary for Halloween',
+            'A Square Accent in Etching (Goldwork)',
+            'A Jacobean Enchanted Blossoming Medallion Top of the Line -',
+            'Fantastic, Marvelous, Blooming Dinosaurs Lightening',
+            'Southwestern (Trapunto) Delightful Monarch Dazzling Delicate',
+            'An Applique with (Crafty Cut Applique)',
+            'Ballerina Bouquet Sunflower Schematic Balloon Impossible',
+            'Hummingbird Snowflake Wonderland'
+            'Sunday Monday Tuesday Wednesday Thursday Friday Saturday',
+            'Baroque Goldfinch Botanicals Kaleidoscope Ironwork Moroccan',
+            'Quilting Nouveau Japanese Dragonfly Birdhouse Majestic American',
+            'Exquisite Space - Space Lighthouse Starfish Seafood Adventure',
+            'Patchwork Free-Flying Rainforest Madagascan Forget-Me-Not',
+            'Dragonflies',
             'No changes'
             ]
     for i in names:
